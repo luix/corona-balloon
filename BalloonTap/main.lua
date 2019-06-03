4093e965bf4d03ed3dea8e21c29f301b1e81aecb
LuiX <luis.a.vivero@gmail.com>
2019-06-02 20:52:19 -0700

    Loading the balloon with 80% of transparency.

--- BalloonTap/main.lua
+++ BalloonTap/main.lua
@@ -11,3 +11,8 @@ background.y = display.contentCenterY
 local platform = display.newImageRect( "images/platform.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight - 25
+
+local balloon = display.newImageRect( "images/balloon.png", 112, 112 )
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY
+balloon.alpha = 0.8
