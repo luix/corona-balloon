f9ef3c9567a31a8467e62124f5226b2bcda56ce1
LuiX <luis.a.vivero@gmail.com>
2019-06-02 20:48:41 -0700

    Loading the platform

--- BalloonTap/main.lua
+++ BalloonTap/main.lua
@@ -7,3 +7,7 @@
 local background = display.newImageRect( "images/background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
+
+local platform = display.newImageRect( "images/platform.png", 300, 50 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight - 25
