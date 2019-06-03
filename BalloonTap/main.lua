59cecc76b795a00c01203925ac8f495d1ca6fcd5
LuiX <luis.a.vivero@gmail.com>
2019-06-02 20:44:44 -0700

    Positioning the image at the correct location on the screen.

--- BalloonTap/main.lua
+++ BalloonTap/main.lua
@@ -5,3 +5,5 @@
 -----------------------------------------------------------------------------------------
 
 local background = display.newImageRect( "images/background.png", 360, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
