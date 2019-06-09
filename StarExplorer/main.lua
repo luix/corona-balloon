f86b351eaabf713918339cd14124dabd7720bf2a
LuiX <luis.a.vivero@gmail.com>
2019-06-08 22:43:08 -0700

    Fixing my dislexia typos in the code

--- StarExplorer/main.lua
+++ StarExplorer/main.lua
@@ -99,26 +99,26 @@ local function createAsteriod()
     physics.addBody( newAsteroid, "dynamic", { radius=40, bounce=0.8 } )
     newAsteroid.myName = "asteroid"
 
-    local whereForm = math.random( 3 )
+    local whereFrom = math.random( 3 )
 
-    if ( whereForm == 1 ) then
-        -- from the left
+    if ( whereFrom == 1 ) then
+        -- From the left
         newAsteroid.x = -60
         newAsteroid.y = math.random( 500 )
         newAsteroid:setLinearVelocity( math.random( 40,120 ), math.random( 20,60 ) )
-    elseif ( whereForm == 2 ) then
-        -- from the top
+    elseif ( whereFrom == 2 ) then
+        -- From the top
         newAsteroid.x = math.random( display.contentWidth )
         newAsteroid.y = -60
         newAsteroid:setLinearVelocity( math.random( -40,40 ), math.random( 40,120 ) )
-    elseif ( whereForm == 3 ) then
-        -- from the right
+    elseif ( whereFrom == 3 ) then
+        -- From the right
         newAsteroid.x = display.contentWidth + 60
         newAsteroid.y = math.random( 500 )
         newAsteroid:setLinearVelocity( math.random( -120,-40 ), math.random( 20,60 ) )
     end
 
-    newAsteroid:appplyTorque( math.random( -6,6 ) )
+    newAsteroid:applyTorque( math.random( -6,6 ) )
 end
 
 
