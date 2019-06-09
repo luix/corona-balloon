0bea8b1bb719444f8c6e685b2cd085ede48d0382
LuiX <luis.a.vivero@gmail.com>
2019-06-09 07:43:44 -0700

    Add functions to gotoGame() and gotoHighScores().

--- StarExplorer/menu.lua
+++ StarExplorer/menu.lua
@@ -8,8 +8,13 @@ local scene = composer.newScene()
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
 
+local function gotoGame()
+    composer.gotoScene( "game" )
+end
 
-
+local function gotoHighScores()
+    composer.gotoScene( "highscores" )
+end
 
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
