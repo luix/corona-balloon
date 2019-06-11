2b92aba8d2d6cc0c1378bb297603d7786144999d
LuiX <luis.a.vivero@gmail.com>
2019-06-10 20:01:27 -0700

    Scene cleanup.

--- StarExplorer/game.lua
+++ StarExplorer/game.lua
@@ -310,6 +310,7 @@ function scene:hide( event )
 		-- Code here runs immediately after the scene goes entirely off screen
     Runtime:removeEventListener( "collision", onCollision )
     physics.pause()
+    composer.removeScene( "game" )
 	end
 end
 
