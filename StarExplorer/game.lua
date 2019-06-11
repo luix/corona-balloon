0854407b82b20229966543e7382e64c15b6cc8d6
LuiX <luis.a.vivero@gmail.com>
2019-06-10 19:09:47 -0700

    Create the ship and both text objects.

--- StarExplorer/game.lua
+++ StarExplorer/game.lua
@@ -255,6 +255,16 @@ function scene:create( event )
   local background = display.newImageRect( backGroup, "background.png", 800, 1400 )
   background.x = display.contentCenterX
   background.y = display.contentCenterY
+
+  ship = display.newImageRect( mainGroup, objectSheet, 4, 98, 79 )
+  ship.x = display.contentCenterX
+  ship.y = display.contentHeight - 100
+  physics.addBody( ship, { radius=30, isSensor=true } )
+  ship.myName = "ship"
+
+  -- Display lives and score
+  livesText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+  scoreText = display.newText( uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36 )
 end
 
 
