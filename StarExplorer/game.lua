3ce266653e8046edc3b7c1c0289b3d3430fd8146
LuiX <luis.a.vivero@gmail.com>
2019-06-10 19:13:30 -0700

    Add the 'tap' and 'touch' event listeners to the ship.

--- StarExplorer/game.lua
+++ StarExplorer/game.lua
@@ -265,6 +265,9 @@ function scene:create( event )
   -- Display lives and score
   livesText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36 )
   scoreText = display.newText( uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36 )
+
+  ship:addEventListener( "tap", fireLaser )
+  ship:addEventListener( "touch", dragShip ) 
 end
 
 
